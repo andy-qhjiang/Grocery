ca73b72a98ffe74d22554ff958158a8c7251c49f
qhjiang <1155228873@link.cuhk.edu.hk>
2024-12-27 17:01:50 +0800

    uploaded from workPC

--- code_finite_difference/exer1_3_1.m
+++ code_finite_difference/exer1_3_1.m
@@ -1,20 +1,21 @@
 % This is a program solving exercise1_3_1
+% For values of x = -1:0.01:3, t = 0:0.01:2.4, 
+% we are going to solve the wave equation u_t + u_x = 0
 
 %% initialize the wave
 clc, clear
 % Define spatial domains and time parameters
 h_values = [1/10, 1/20, 1/40];
 t_final = 2.4;
-    
-speed = 1;
-h = h_values(2);
+speed = -1;
+h = h_values(3);
 x = -1:h:3;
-k = 0.8 * h;  % Calculating time-step based on lambda
-t = 0:k:t_final;
 x_standard = -1:0.01:3; % this is used to draw the original function u0 for contrast in figure 3
 
 % Initialize u with initial condition
-u0 = initial_fun(x);
+% type = 1: u(x, 0) = cos(pi * x)^2
+% type = 2: u(x, 0) = 1 - 2 * sign(x) * x
+u0 = initial_fun(x, 1);
 
 figure(1)
 clf
@@ -23,6 +24,11 @@ axis equal
 plot(x, u0, 'o-');
 
 %% solve v^{n}_{m}
+
+% set up time spacing based on lambda
+lambda = 0.8;
+k = lambda*h;
+t = 0: k :2.4;
 
 u = zeros(length(t), length(x)); % grid function v^{n}_{m}
 u(1, :) = u0; % initial condition
@@ -36,21 +42,20 @@ u(:, 1) = zeros(length(t), 1);
 % scheme 4: leapfrog
 % scheme 5: Lax-Friedrichs
 
-scheme_index = 4; % different schemes as needed
-u = scheme_computation(scheme_index, u, speed, 0.8);
+scheme_index = 5; % different schemes as needed
+u = scheme_computation(scheme_index, u, speed, lambda);
 
 
 %% Create a 3D surface plot of the solution
-
 figure(2)
 clf
 axis equal
 hold on
 
 
-for i = 30:10:length(t) % choose larger spacing for a better view
+for i = 15:10:60 % choose larger spacing for a better view
     plot3(x, t(i)*ones(length(x), 1), u(i, :), 'o-', 'LineWidth', 1.5)
-    plot3(x_standard, t(i)*ones(length(x_standard), 1), initial_fun(x_standard - speed*t(i)*ones(1,length(x_standard))), 'k-', 'LineWidth', 1)
+    plot3(x_standard, t(i)*ones(length(x_standard), 1), initial_fun(x_standard - speed*t(i)*ones(1,length(x_standard)), 1), 'k-', 'LineWidth', 1)
 end
 
 title(['3D Surface Plot for h = ', num2str(h)]);
@@ -58,6 +63,9 @@ xlabel('Space (x)');
 ylabel('Time (t)');
 zlabel('Amplitude');
 view([-34 27]);
+
+% Set the range of the y-axis
+ylim([0.5, 2.5]);
 
 
 
--- code_finite_difference/initial_fun.m
+++ code_finite_difference/initial_fun.m
@@ -1,14 +1,26 @@
-function y = initial_fun(x)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-    y = zeros(1, length(x));
-    for i = 1: length(x)
-        if abs(x(i)) <=0.5
-            % y(i) = cos(pi* x(i))^2;
-            y(i) = 1- 2*sign(x(i))*x(i);
+% This is intial function for the wave equation, 
+% g(x) = v(0,x) for t=0.
+function y = initial_fun(x, choice)
+    % Initialize the output array
+    y = zeros(size(x));
+    
+    % Loop through each element in x
+    for i = 1:length(x)
+        if abs(x(i)) <= 0.5
+            switch choice
+                case 1
+                    % First choice: y(i) = cos(pi * x(i))^2
+                    y(i) = cos(pi * x(i))^2;
+                case 2
+                    % Second choice: y(i) = 1 - 2 * sign(x(i)) * x(i)
+                    y(i) = 1 - 2 * sign(x(i)) * x(i);
+                otherwise
+                    % Default choice: another function
+                    y(i) = sin(2 * pi * x(i)); % Example of another choice
+            end
+        else
+            % Set y(i) to 0 if |x(i)| > 0.5
+            y(i) = 0;
         end
     end
-
-   y(abs(y) < 1e-8) = 0; 
-  
 end
