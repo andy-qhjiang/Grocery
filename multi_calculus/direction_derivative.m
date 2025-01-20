22cd7c759b38dc03d13d9d1c51041caeab036c04
qhjiang <1155228873@link.cuhk.edu.hk>
2025-01-20 17:15:58 +0800

    Update direction_derivative.m

--- multi_calculus/direction_derivative.m
+++ multi_calculus/direction_derivative.m
@@ -31,7 +31,7 @@ grid on
 curve_x = linspace(-0.5, 4.5, 500);
 curve_y = 2 * curve_x - 4;
 curve_z = curve_x.^2 + curve_y.^2;
-plot3(curve_x, curve_y, curve_z, 'r', 'LineWidth', 2, 'DisplayName', 'Intersection Curve');
+plot3(curve_x, curve_y, curve_z, 'r', 'LineWidth', 3, 'DisplayName', 'Intersection Curve');
 
 % Plot the plane 2x-y-4=0 in gray style
 plane_X = repmat(curve_x, 300, 1);
@@ -39,7 +39,14 @@ plane_Y = 2.*plane_X - 4;
 z_range = linspace(-0.2, 40, 300);
 plane_Z = repmat(z_range', 1, size(plane_X, 2));
 
-surf(plane_X, plane_Y, plane_Z, 'FaceAlpha', 0.3, 'FaceColor', 'green');
+surf(plane_X, plane_Y, plane_Z, ...
+    'FaceColor', [0.7 0.7 0.7], ... % Light gray color
+    'EdgeColor', 'none', ... % Remove black edges
+    'FaceAlpha', 0.8); % More transparent
+
+quiver3([2 2], [0 0], [4 4], [1 1], [0 2], [0 0], 'k', 'LineWidth', 2, 'DisplayName', 'Direction of dx and ds');
+text(2.5, -0.1, 4, 'dx');
+text(2.6, 1.1, 4, 'ds');
 
 % Labels and legend
 xlabel('X');
