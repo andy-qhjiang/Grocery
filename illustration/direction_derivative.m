09082f7cae062bf26483ef079761a08719469f45
qhjiang <1155228873@link.cuhk.edu.hk>
2025-03-03 18:05:36 +0800

    Uploaded from workPC

--- illustration/direction_derivative.m
+++ illustration/direction_derivative.m
@@ -33,9 +33,9 @@ curve_z = curve_x.^2 + curve_y.^2;
 plot3(curve_x, curve_y, curve_z, 'r', 'LineWidth', 3);
 
 % Plot the plane 2x-y-4=0 in gray style
-plane_X = repmat(curve_x, 300, 1);
+plane_X = repmat(curve_x, 100, 1);
 plane_Y = 2.*plane_X - 4;
-z_range = linspace(-0.2, 40, 300);
+z_range = linspace(-0.2, 40, 100);
 plane_Z = repmat(z_range', 1, size(plane_X, 2));
 
 surf(plane_X, plane_Y, plane_Z, ...
@@ -43,6 +43,7 @@ surf(plane_X, plane_Y, plane_Z, ...
     'EdgeColor', 'none', ... % Remove black edges
     'FaceAlpha', 0.6); %  smaller parameter is more transparent
 
+plot3(2, 0, 4, 'ko', 'MarkerFaceColor', 'k');
 quiver3([2 2], [0 0], [4 4], [1 1], [0 2], [0 0], 'k', 'LineWidth', 1);
 text(2.5, -0.1, 4, 'dx');
 text(2.6, 1.1, 4, 'ds');
@@ -51,5 +52,6 @@ text(2.6, 1.1, 4, 'ds');
 xlabel('X');
 ylabel('Y');
 zlabel('Z');
-legend('surface x^2+y^2', 'Intersection Curve', 'plane 2x-y=4', 'Direction of dx and ds');
+title('Directional derivative of x^2+y^2 at (2, 0) along (1,2)');
+legend('surface x^2+y^2', 'Intersection Curve', 'plane 2x-y=4', 'point (2,0,4)', 'differential dx and ds');
 
