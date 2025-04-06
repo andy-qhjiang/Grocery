40873859e16c0bc4da519f1e84e7753f9cf35ad4
qhjiang <1155228873@link.cuhk.edu.hk>
2025-04-06 19:40:51 +0800

    uploaded from workPC

--- OPTIMIZATION/test1.m
+++ OPTIMIZATION/test1.m
@@ -1,7 +1,10 @@
 clearvars
-A = [17 2; 2 7];
-b = [2; 2];
-x0= [0; 0];
+%A = [17 2; 2 7];
+%b = [2; 2];
+%x0= [0; 0];
+A = [1 0; 0 10];
+b = [0; 0];
+x0 = [10; 1];
 
 x_star = A\b;
 tol = 10^-6;
@@ -12,22 +15,24 @@ iterPoint2 = [x0, conj_grad(A, b, x0, tol)];
 
 
 
-f = @(x, y) -2*x - 2*y + 0.5 * (17*x.^2 + 4*x.*y + 7*y.^2);
+f = @(x, y, A, b) -b(1)*x - b(2)*y + 0.5 * (A(1,1)*x.^2 + 2*A(1,2)*x.*y + A(2,2)*y.^2);
 % Create a grid of x and y values
-x = linspace(0, 0.2, 100); % Adjust the range as needed
-y = linspace(0, 0.3, 100);
+x = linspace(-2, 10, 100); % Adjust the range as needed
+y = linspace(-2, 2, 100);
 [X, Y] = meshgrid(x, y);
 
 % Evaluate the function at the grid points
-Z = f(X, Y);
+Z = f(X, Y, A, b);
 
-% Plot the contour
+%% Plot the contour
 figure;
 hold on
-contour(X, Y, Z, 20);        % Draw 20 equidistant contour levels
-plot(iterPoint1(1,:), iterPoint1(2,:), 'o-', 'MarkerFaceColor', 'r');
-plot(iterPoint2(1,:), iterPoint2(2,:), 'diamond--', 'MarkerFaceColor', 'g');
-title('Contour Plot of f(x, y)');
+contour(X, Y, Z, 30);        % Draw 20 equidistant contour levels
+plot(iterPoint1(1,:), iterPoint1(2,:), 'or-', 'MarkerSize',2);
+plot(iterPoint2(1,:), iterPoint2(2,:), 'diamond g--', 'MarkerFaceColor', 'g');
+plot([10, 0], [1, 0], '*--', 'MarkerSize', 8,'LineWidth', 2);
+legend('contour of quadratic form', 'Grident descent', 'Conjugate descent', 'Newton method');
+title('Comparison between descent methods');
 xlabel('x');
 ylabel('y');
 
