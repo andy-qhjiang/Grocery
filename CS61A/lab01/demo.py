08033b2cc621319df55ddb182deeaaf50753950d
andy-qhjiang <lingjun100010101@gmail.com>
2025-05-30 16:27:45 +0800

    Add files via upload

---
+++ CS61A/lab01/demo.py
@@ -0,0 +1,22 @@
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    while n>0:
+        if (n//10) % 10 ==8 and n % 10 == 8:
+            return True
+        n //=10
+
+    return False
